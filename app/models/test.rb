6deb7527882082b373efe054b1f75ca28dde2e09
Slava <slavik.tkachenko@gmail.com>
2019-08-04 15:28:23 +0600

    update er, fix dependency, fix reference

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,11 +2,9 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: :User, foreign_key: :author_id
 
-  has_many :questions, dependent: :nullify
+  has_many :questions, dependent: :destroy
   has_many :tests_users
   has_many :users, through: :tests_users
-  # belongs_to :created_tests, class_name: 'User', foreign_key: 'author_id'
-
 
   def self.sort_desc_test_name_by_category(title)
     joins(:category).where(categories: {title: title}).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
-  # has_many :tests, inverse_of: 'created_tests'
 
   def show_passed_tests_by_level(level)
     tests.where(level: level)
