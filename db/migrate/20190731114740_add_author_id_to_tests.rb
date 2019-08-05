c1f9a0137bceb6c4b2e52d169b8526f06646eae7
Slava <slavik.tkachenko@gmail.com>
2019-08-05 10:35:15 +0600

    add table reference

--- db/migrate/20190731114740_add_author_id_to_tests.rb
+++ db/migrate/20190731114740_add_author_id_to_tests.rb
@@ -1,6 +1,5 @@
 class AddAuthorIdToTests < ActiveRecord::Migration[5.2]
   def change
-    add_column :tests, :author_id, :integer
-    add_index :tests, :author_id
+    add_reference :tests, :author, foreign_key: {to_table: :users}
   end
 end
