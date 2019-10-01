a14ccf896594b526a55608f00a9005820f7ecc3a
Slava <slavik.tkachenko@gmail.com>
2019-10-01 15:37:38 +0600

    upgrade test controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,22 +1,54 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: :show
-  before_action :find_question, only: :show
+  before_action :find_test, only: [:show, :edit, :update, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
     @tests = Test.all
   end
 
-  def show
-    
+  def show; end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to test_path
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to test_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @test.destroy!
+    redirect_to tests_path
   end
 
   private
+
+  def rescue_with_test_not_found
+    render plain: 'Test was not found'
+  end
 
   def find_test
     @test = Test.find(params[:id])
   end
 
-  def find_question
-    @question = Question.find(params[:id])
+  def test_params
+    params.require(:test).permit(:title, :level, :author_id, :category_id)
   end
 end
