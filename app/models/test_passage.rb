4afff0652e5c43aebad415ef0e1204a440c594bc
Slava <slavik.tkachenko@gmail.com>
2019-10-10 11:11:50 +0600

    fix styles, refactor routes, refactor score_counter, minor fixes

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,26 +5,19 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :set_first_question, on: %i[create update]
+  before_validation :set_next_question, on: %i[create update]
 
   def completed?
     current_question.nil?
   end
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    # self.current_question = next_question
     save!
   end
 
   def current_question_number
-    # puts '!!!!!!!!!!!!!!!!!!'
-    # puts test.questions.inspect
-    # puts current_question.id
-    # puts '!!!!!!!!!!!!!!!!!!'
     test.questions.order(:id).where('id <= ?', current_question.id).count
   end
 
@@ -32,15 +25,11 @@ class TestPassage < ApplicationRecord
     score_counter >= SUCCESS_PERCENT
   end
 
-  def final_score
-    score_counter.to_i
-  end
-
-  private
-
   def score_counter
     (correct_questions * 100) / test.questions.count
   end
+
+  private
 
   def correct_answer?(answer_ids)
     correct_answers_count = correct_answers.count
@@ -48,7 +37,7 @@ class TestPassage < ApplicationRecord
     (correct_answers_count == correct_answers.where(id: answer_ids).count) && correct_answers_count == answer_ids.count
   end
 
-  def set_first_question
+  def set_next_question
     if new_record?
       self.current_question = test.questions.first
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :test_passages
+  has_many :test_passages, dependent: :nullify
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   end
 
   def test_passage(test)
-    test_passages.order(id: :desc).find_by(test_id: test.id)
+    test_passages.order(id: :desc).find_by(test: test)
   end
 end
