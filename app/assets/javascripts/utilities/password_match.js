44cf424451331bcb88006d34fea374f309e870d3
Slava <slavik.tkachenko@gmail.com>
2019-11-06 13:08:05 +0600

    remove matchCondition

--- app/assets/javascripts/utilities/password_match.js
+++ app/assets/javascripts/utilities/password_match.js
@@ -3,19 +3,13 @@ var match = function() {
   var confirmPasswordElement = document.getElementById('confirm_password').value
   var passwordElement = document.getElementById('password').value
 
-  if (confirmPasswordElement == passwordElement && matchCondition()) {
+  if (confirmPasswordElement == passwordElement) {
       matchResult('#5cb85c', 'matching')
   } 
-  else if (confirmPasswordElement != passwordElement && matchCondition()) {
+  else if (confirmPasswordElement != passwordElement) {
       matchResult('#d9534f', 'not matching')
   }
-
-  function matchCondition() {
-    if (confirmPasswordElement.length != 0 && passwordElement.length != 0) {
-      return true
-    }
-  }
-
+  
   function matchResult(color, message) {
     document.getElementById('message').style.color = color;
     document.getElementById('message').innerHTML = message;
