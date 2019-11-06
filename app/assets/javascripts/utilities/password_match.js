5b079cb37131032bb5e3913c6201f1a40700c760
Slava <slavik.tkachenko@gmail.com>
2019-11-06 11:54:51 +0600

    add password match js function, add onkeyup event to registration password & confirmation password input

--- app/assets/javascripts/utilities/password_match.js
+++ app/assets/javascripts/utilities/password_match.js
@@ -1,24 +1,25 @@
 // Password and Password Confirm match
-var check = function() {
-  if (document.getElementById('password').value ==
-    document.getElementById('confirm_password').value && document.getElementById('confirm_password').value.length != 0) {
-    // document.getElementById('message').style.color = 'green';
-    // document.getElementById('message').innerHTML = 'matching';
-    document.getElementById('password').style.borderColor = '#5cb85c';
-    document.getElementById('confirm_password').style.borderColor = '#5cb85c';
-  } else if (document.getElementById('password').value !=
-      document.getElementById('confirm_password').value && document.getElementById('password').value.length != 0) {
-    // document.getElementById('message').style.color = 'red';
-    // document.getElementById('message').innerHTML = 'not matching';
-    document.getElementById('password').style.borderColor = '#d9534f';
-    document.getElementById('confirm_password').style.borderColor = '#d9534f';
-  } else {
-    document.getElementById('password').style.borderColor = '#5cb85c';
-    document.getElementById('confirm_password').style.borderColor = '#5cb85c';
+var match = function() {
+  var confirmPasswordElement = document.getElementById('confirm_password')
+  var passwordElement = document.getElementById('password')
+
+  if (confirmPasswordElement.value == passwordElement.value && matchCondition()) {
+      matchResult('#5cb85c', 'matching')
+  } 
+  else if (confirmPasswordElement.value != passwordElement.value && matchCondition()) {
+      matchResult('#d9534f', 'not matching')
+  }
+
+  function matchCondition() {
+    if (confirmPasswordElement.value.length != 0 && passwordElement.value.length != 0) {
+      return true
+    }
+  }
+
+  function matchResult(color, message) {
+    document.getElementById('message').style.color = color;
+    document.getElementById('message').innerHTML = message;
+    passwordElement.style.borderColor = color;
+    confirmPasswordElement.style.borderColor = color;
   }
 }
-
-// create green function
-// create  red function
-// empty password filled confirmation shows green - to fix
-// add else logic???
