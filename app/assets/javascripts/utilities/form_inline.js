cea47abb2d757219562c8e3bd7da6383a7a98844
Slava <slavik.tkachenko@gmail.com>
2019-11-15 13:45:01 +0600

    remove error compiler from js code for debug

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,12 +1,18 @@
 document.addEventListener('turbolinks:load', function() {
-  $('.form-inline-link').on('click', formInlineLinkHandler)
+  var controls = document.querySelectorAll('.form-inline-link')
 
-  var errors = document.querySelector('.resource-errors')
-
-  if (errors) {
-    var resourceId = errors.dataset.resourceId
-    formInlineHandler(resourceId)
+  if(controls.length) {
+    for (var i = 0; i < controls.length; i++) {
+      controls[i]. addEventListener('click', formInlineLinkHandler)
+    }
   }
+
+  // var errors = document.querySelector('.resource-errors')
+
+  // if (errors) {
+    // var resourceId = errors.dataset.resourceId
+    // formInlineHandler(resourceId)
+  // }
 })
 
 function formInlineLinkHandler(event) {
@@ -17,18 +23,17 @@ function formInlineLinkHandler(event) {
 }
 
 function formInlineHandler(testId) {
-  var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
-  var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
+  var link = document.querySelector('form-inline-link[data-test-id="' + testId + '"]')
+  var testTitle = document.querySelector('test-title[data-test-id="' + testId + '"]')
+  var formInline = document.querySelector('form-inline[data-test-id="' + testId + '"]')
 
-  $formInline.toggle()
-  $testTitle.toggle()
-
-  if (link) {
-    if ($formInline.is(':visible')) {
-      link.textContent = 'Cancel'
-    } else {
-      link.textContent = 'Edit'
-    }
+  if(formInline.classList.contains('hide')) {
+    testTitle.classList.add('hide')
+    formInline.classList.remove('hide')
+    link.textContent = 'Cancel'
+  } else {
+    testTitle.classList.remove('hide')
+    formInline.classList.hide('hide')
+    link.textContent = 'Edit'
   }
 }
