03391fb17fb3b66a090cdf5ac93b4941cacb02d6
Slava <slavik.tkachenko@gmail.com>
2019-11-22 14:56:13 +0600

    fix rubocop warning

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,7 +9,7 @@ class Test < ApplicationRecord
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, uniqueness: { scope: :level,
-    message: "only one test of this kind per level" }
+                                  message: 'only one test of this kind per level' }
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
