e10821d933561bf148bc2dc3f35622ecef863c37
Slava <slavik.tkachenko@gmail.com>
2019-12-12 17:46:11 +0600

    refactor rules

--- app/services/badge_rules/grand_master.rb
+++ app/services/badge_rules/grand_master.rb
@@ -1,32 +1,18 @@
 module BadgeRules
   class GrandMaster < BaseRule
     def call
-      if @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length < Test.in_category(@badge.value.split(",").map { |s| s.to_i }).count && @user.earned_badges.where(badge_id: @badge.id).count == 0
+      group_length = @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length
+      scope_count = Test.in_category(@badge.value.split(",").map { |s| s.to_i }).count
+      badge_count = @user.earned_badges.where(badge_id: @badge.id).count
+      group_min = @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min
+
+      if group_length < scope_count && badge_count == 0
         false
-      elsif @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length == Test.in_category(@badge.value.split(",").map { |s| s.to_i }).count
-        @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min == @user.earned_badges.where(badge_id: @badge.id).count + 1
+      elsif group_length == scope_count
+        group_min == badge_count + 1
       else
-        @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min >= @user.earned_badges.where(badge_id: @badge.id).count + 1
+        group_min >= badge_count + 1
       end
     end
   end
 end
-
-# module BadgeRules
-#   class GrandMaster < BaseRule
-#     def call
-#       group_length = @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length
-#       group_min = @user.test_passages.in_category(@badge.value.split(",").map { |s| s.to_i }).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min
-#       scope_count = Test.in_category(@badge.value.split(",").map { |s| s.to_i }).count
-#       badges_count = @user.earned_badges.where(badge_id: @badge.id).count
-
-#       if group_length < scope_count && badges_count == 0
-#         false
-#       elsif group_length == scope_count
-#         group_min == (badges_count + 1)
-#       else
-#         group_min >= (badges_count + 1)
-#       end
-#     end
-#   end
-# end
--- app/services/badge_rules/master.rb
+++ app/services/badge_rules/master.rb
@@ -1,12 +1,17 @@
 module BadgeRules
   class Master < BaseRule
     def call
-      if @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length < Test.easy.count && @user.earned_badges.where(badge_id: @badge.id).count == 0
+      group_length = @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length
+      scope_count = Test.easy.count
+      badge_count = @user.earned_badges.where(badge_id: @badge.id).count
+      group_min = @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min
+
+      if group_length < scope_count && badge_count == 0
         false
-      elsif @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length == Test.easy.count
-        @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min == @user.earned_badges.where(badge_id: @badge.id).count + 1
+      elsif group_length == scope_count
+        group_min == badge_count + 1
       else
-        @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val.count }.min >= @user.earned_badges.where(badge_id: @badge.id).count + 1
+        group_min >= badge_count + 1
       end
     end
   end
