2a0b2ca32accb213123409d52f5bd22ae21bd591
Slava <slavik.tkachenko@gmail.com>
2019-12-13 12:14:12 +0600

    add rule validation of the same value, add pre-check for rules to reduce db request time

--- app/services/badge_rules/category_master.rb
+++ app/services/badge_rules/category_master.rb
@@ -1,6 +1,8 @@
 module BadgeRules
   class CategoryMaster < BaseRule
     def call
+      @test_passage.test.category_id == @badge.value.to_i
+
       group_length = @user.test_passages.in_category(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length
       scope_count = Test.in_category(@badge.value.to_i).count
       badge_count = @user.earned_badges.where(badge_id: @badge.id).count
--- app/services/badge_rules/master.rb
+++ app/services/badge_rules/master.rb
@@ -1,6 +1,8 @@
 module BadgeRules
   class Master < BaseRule
     def call
+      @test_passage.test.level == @badge.value.to_i
+
       group_length = @user.test_passages.with_level(@badge.value.to_i).success.pluck(:title).group_by { |elem| elem }.map { |key, val| val }.length
       scope_count = Test.easy.count
       badge_count = @user.earned_badges.where(badge_id: @badge.id).count
